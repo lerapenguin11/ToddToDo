6760e78f4572eedda0a9a044c2a269e500d36d6a
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-09-08 16:36:38 +0500

    screen layout schedules

--- app/src/main/java/com/example/toddtodo/presentation/MenuFragment.kt
+++ app/src/main/java/com/example/toddtodo/presentation/MenuFragment.kt
@@ -6,14 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.toddtodo.R
+import com.example.toddtodo.databinding.FragmentMenuBinding
+import com.example.toddtodo.utilits.replaceFragment
 
 class MenuFragment : Fragment() {
-
+    private var _binding : FragmentMenuBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.blockScheduled.setOnClickListener {
+            replaceFragment(ScheduledFragment())
+        }
     }
 }
