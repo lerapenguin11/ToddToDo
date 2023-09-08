33982021ed4a98ac8f6e9b1a5aee642b44f688fc
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-09-08 14:04:21 +0500

    project setup

---
+++ app/src/main/java/com/example/toddtodo/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.toddtodo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.toddtodo.R
+import com.example.toddtodo.databinding.ActivityMainBinding
+import com.example.toddtodo.utilits.APP_ACTIVITY
+import com.example.toddtodo.utilits.setStatusBarGradiant
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        APP_ACTIVITY = this
+        setStatusBarGradiant(this)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/toddtodo/utilits/Const.kt
@@ -0,0 +1,5 @@
+package com.example.toddtodo.utilits
+
+import com.example.toddtodo.presentation.MainActivity
+
+lateinit var APP_ACTIVITY : MainActivity
